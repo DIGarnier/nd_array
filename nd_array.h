ac54bb9c29cfb32ffd1954f25c5f1366496ac8e1
david garnier <davidigarnier@gmail.com>
2018-09-27 20:27:23 -0400

    init commit

---
+++ nd_array.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <array>
+
+
+template <typename T, int... N>
+struct multi_array {
+
+std::array<T, (N * ...)> array = {};
+    std::array<T, sizeof...(N) + 1> dims{1, N...};
+    decltype(std::make_index_sequence<sizeof...(N)>{}) seq = std::make_index_sequence<sizeof...(N)>{};
+
+    template <typename... Idxs>
+    constexpr T const& at(Idxs... indice) const {
+        range_check(indice...);
+        return array[calculate_index(seq, indice...)];
+    }
+
+    template <typename... Idxs>
+    T& at(Idxs... indice) {
+        range_check(indice...);
+        return array[calculate_index(seq, indice...)];
+    }
+
+private:
+    template <typename... Idxs, size_t... Is>
+    constexpr size_t calculate_index(std::index_sequence<Is...>,
+                                     Idxs... idx) const {
+        return ((dims[std::max((T)(Is - 1), 0)] * dims[Is] * idx) + ...);
+    }
+
+    template <typename... Idxs, size_t... Is>
+    constexpr void range_check(Idxs... idx) const {
+        if (not_in_range(seq, idx...))
+            throw std::out_of_range("Indices out of range");
+    }
+
+    template <typename... Idxs, size_t... Is>
+    constexpr bool not_in_range(std::index_sequence<Is...>,
+                                Idxs... idx) const {
+        return ((idx >= dims[Is + 1]) || ...);
+    }
+
+};
