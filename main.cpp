299ed69be7846eb9548fbf863de482d11456d558
DIGarnier <DIGarnier@users.noreply.github.com>
2024-12-10 00:14:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,31 +2,34 @@
 #include "nd_array.h"
 
 
-
-int main(){
+int main()
+{   
     constexpr int xMax = 3;
     constexpr int yMax = 2;
     constexpr int zMax = 2;
+    constexpr auto ndarray = nd_array<int, xMax, yMax, zMax>{
+        1,2,3,
+        4,5,6,
 
-    auto test = nd_array<int, xMax, yMax, zMax>{    1,2,3,
-                                                    4,5,6,
+        7,8,9,
+        10,11,12
+    };
 
-                                                    7,8,9,
-                                                    10,11,12
-                                               };
-    
-    for(auto e: test)
-        std::cout << e << '\n';
+    static_assert(ndarray.at(0,0,0) == 1);
+    static_assert(ndarray.at(1,0,0) == 2);
+    static_assert(ndarray.at(2,0,0) == 3);
 
+    static_assert(ndarray.at(0,1,0) == 4);
+    static_assert(ndarray.at(1,1,0) == 5);
+    static_assert(ndarray.at(2,1,0) == 6);
 
-    
-    for(auto i = 0; i < test.size(); ++i){
-        auto idx = i;
-        int z = idx / (xMax * yMax);
-        idx -= (z * xMax * yMax);
-        int y = idx / xMax;
-        int x = idx % xMax;
-        std::cout << "x: " << x << " y: " << y << " z: " << z << " result: " << test.at(x, y, z) << '\n';
-    }
-    
-}
+    static_assert(ndarray.at(0,0,1) == 7);
+    static_assert(ndarray.at(1,0,1) == 8);
+    static_assert(ndarray.at(2,0,1) == 9);
+
+    static_assert(ndarray.at(0,1,1) == 10);
+    static_assert(ndarray.at(1,1,1) == 11);
+    static_assert(ndarray.at(2,1,1) == 12);
+
+    return 0;
+}     
